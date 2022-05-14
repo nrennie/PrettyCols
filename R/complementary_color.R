bc611223b1bb4f12d78e0052f8121a1a522454d6
nrennie <nrennie35@gmail.com>
2022-05-14 18:38:54 +0100

    add complimentary function

---
+++ R/complementary_color.R
@@ -0,0 +1,18 @@
+#' Find complementary color of a hex code
+#'
+#' @param hex Hex code given as a string
+#' @return c_col hex code of complementary color
+#'
+#' @export
+
+complementary_color <- function(hex) {
+  # convert to HSL
+  hsl_col = plotwidgets::col2hsl(hex)
+  # Add 180 degrees to hue
+  hsl_new = hsl_col
+  hsl_new[1,1] = (hsl_new[1,1] + 180) %% 360
+  # Convert back to hex
+  c_col = plotwidgets::hsl2col(hsl_new)
+  return(c_col)
+}
+
---
+++ R/complementary_colour.R
@@ -0,0 +1,11 @@
+#' Find complementary colour of a hex code
+#'
+#' @param hex Hex code given as a string
+#' @return c_col hex code of complementary colour
+#'
+#' @export
+
+complementary_colour <- complementary_color
+
+
+
