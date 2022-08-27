fb8cd44a26076b75ca0565ed52043582c8a3835b
nrennie <nrennie35@gmail.com>
2022-08-27 22:57:36 +0100

    edit palette printing

--- R/view_all_palettes.R
+++ R/view_all_palettes.R
@@ -14,8 +14,8 @@ view_all_palettes <- function(type = "all") {
   } else {
     if (type == "all") {
       n_all <- length(PrettyColsPalettes)
-      n_row <- round(sqrt(n_all))
-      n_col <- ceiling(n_all / n_row)
+      n_col <- min(4, floor(sqrt(n_all)))
+      n_row <- ceiling(n_all / n_col)
       par(mfrow = c(n_row, n_col))
       purrr::map(.x = names(PrettyColsPalettes), .f = ~print(prettycols(.x)))
       par(mfrow = c(1, 1))
@@ -24,8 +24,8 @@ view_all_palettes <- function(type = "all") {
       types <- unlist(lapply(PrettyColsPalettes, `[[`, 3))
       filtered_palettes <- PrettyColsPalettes[unname(which(types == type))]
       n_all <- length(filtered_palettes)
-      n_row <- round(sqrt(n_all))
-      n_col <- ceiling(n_all / n_row)
+      n_col <- min(3, floor(sqrt(n_all)))
+      n_row <- ceiling(n_all / n_col)
       par(mfrow = c(n_row, n_col))
       purrr::map(.x = names(filtered_palettes), .f = ~print(prettycols(.x)))
       par(mfrow = c(1, 1))
