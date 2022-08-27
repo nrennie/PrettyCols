af3213903f4f0c454c9bd5f4c3a5bd81cb0f575f
nrennie <nrennie35@gmail.com>
2022-08-27 22:05:17 +0100

    edit palette printing

--- R/print_palette.R
+++ R/print_palette.R
@@ -10,11 +10,11 @@ print.palette <- function(x, ...) {
   graphics::image(1:n, 1, as.matrix(1:n), col = x,
         ylab = "", xaxt = "n", yaxt = "n", bty = "n")
   graphics::rect(0, 0.92, n + 1, 1.08,
-       col = grDevices::rgb(0, 0, 0, 0.3),
+       col = grDevices::rgb(0, 0, 0, 0.6),
        border = NA)
   graphics::text((n + 1) / 2, 1,
        labels = attr(x, "name"),
-       cex = 2.5,
+       cex = 2,
        family = "sans",
        col = grDevices::rgb(1, 1, 1, 1))
 }
