97fb737267a49edeb6546adf321a7c47355227f6
nrennie <nrennie35@gmail.com>
2022-09-18 17:24:29 +0100

    update tests

--- tests/testthat/test-display.R
+++ tests/testthat/test-display.R
@@ -3,8 +3,9 @@ test_that("palette display", {
   expect_silent(view_all_palettes())
   expect_silent(view_all_palettes(type = "seq"))
 
-  # bad inputs
   expect_error(prettycols("new_name"))
   expect_error(prettycols("Relax", n = 6))
+
+  expect_equal(ncol(colourblind_friendly()), 2)
 
 })
--- tests/testthat/test-scales.R
+++ tests/testthat/test-scales.R
@@ -8,6 +8,12 @@ test_that("scales functions works", {
   colour_scale = scale_colour_pretty_c(name = "Greens")
   expect_equal(colour_scale$is_discrete(), FALSE)
 
+  colour_scale = scale_color_pretty_c(name = "Greens")
+  expect_equal(colour_scale$is_discrete(), FALSE)
+
   colour_scale = scale_colour_pretty_d(name = "Relax")
+  expect_equal(colour_scale$is_discrete(), TRUE)
+
+  colour_scale = scale_color_pretty_d(name = "Relax")
   expect_equal(colour_scale$is_discrete(), TRUE)
 })
